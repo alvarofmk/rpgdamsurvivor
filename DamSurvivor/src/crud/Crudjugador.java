eab89f9d74cbc96d8c0e7af3bb938d067f197c05
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2021-11-25 10:46:35 +0100

    probando como mostrar los stats

--- DamSurvivor/src/crud/Crudjugador.java
+++ DamSurvivor/src/crud/Crudjugador.java
@@ -6,6 +6,7 @@ public class Crudjugador {
 	
 	public model.Jugador crearJugador (String nombre,  int salud, int ataque, int defensa) {
 		model.Jugador p1 = new model.Jugador(nombre, salud, ataque, defensa);
+		System.out.println("Su nombre es: " + p1.getNombre());
 		return p1;
 	}
 	
