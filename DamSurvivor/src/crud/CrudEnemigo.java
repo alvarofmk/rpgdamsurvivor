f45988e5dfc53c962a646f394296aafe2b9ab3e1
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-12-03 12:31:58 +0100

    enemigos

--- DamSurvivor/src/crud/CrudEnemigo.java
+++ DamSurvivor/src/crud/CrudEnemigo.java
@@ -1,5 +1,30 @@
 package crud;
 
-public class CrudEnemigo {
+import model.Enemigo;
 
+public class CrudEnemigo {
+	//AddEnemy
+	//checkVidaMax
+	//atacar
+	//defender
+	
+	public int rafaAtaque1 (Enemigo Rafa) {
+		int ataqueTotal;
+		ataqueTotal = Rafa.getAtk()+10+5; //el 5 debe cambiarse por el valor del dado
+		return ataqueTotal;
+	}
+	
+	public int rafaAtaque2 (Enemigo Rafa) {
+		int ataqueTotal = Rafa.getAtk()+10+20; //el 20 es equivalente al ataque fuerte con el dado.
+	}
+	
+	public int rafaDefensa (Enemigo Rafa) {
+		int defensa = Rafa.getDef(); 
+	}
+	
+	public int rafaCheckVida (Enemigo Rafa) {
+		int checkVida = Rafa.getHp()
+				
+	}
 }
+
