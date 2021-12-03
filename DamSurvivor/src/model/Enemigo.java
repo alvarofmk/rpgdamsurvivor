968247aa78eb1c4973aac5370bcf1f08d380d8bf
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-12-03 12:10:30 +0100

    enemigos

--- DamSurvivor/src/model/Enemigo.java
+++ DamSurvivor/src/model/Enemigo.java
@@ -1,5 +1,55 @@
 package model;
 
 public class Enemigo {
-
+	
+	private String nombre;
+	private int atk;
+	private int def ;
+	private int hp ;
+	private int hpMax ;
+	
+	public Enemigo(String nombre, int atk, int def, int hp, int hpMax) {
+		super();
+		this.nombre = nombre;
+		this.atk = atk;
+		this.def = def;
+		this.hp = hp;
+		this.hpMax = hpMax;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getAtk() {
+		return atk;
+	}
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+	public int getDef() {
+		return def;
+	}
+	public void setDef(int def) {
+		this.def = def;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getHpMax() {
+		return hpMax;
+	}
+	public void setHpMax(int hpMax) {
+		this.hpMax = hpMax;	
+	}
+	@Override
+	public String toString() {
+		return "Enemigo [nombre=" + nombre + ", atk=" + atk + ", def=" + def + ", hp=" + hp + ", hpMax=" + hpMax + "]";
+	}
+	
+	
 }
