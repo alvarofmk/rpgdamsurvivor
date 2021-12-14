050e1d60acb41eb8763adf75f0dc208f6ce48f62
Álvaro <alvarofmk@gmail.com>
2021-12-14 21:42:57 +0100

    Añadidas varias vistas y cambiado el proceso del combate

--- DamSurvivor/src/vista/Menus.java
+++ DamSurvivor/src/vista/Menus.java
@@ -1,5 +1,7 @@
 package vista;
 
+import model.Arma;
+import model.Enemigo;
 import model.Jugador;
 
 public class Menus {
@@ -54,7 +56,107 @@ public class Menus {
 	}
 	
 	public void invalid () {
-		System.out.println("\t\tElige bien.");
+		System.out.println("\t\t\t  -  Elige bien  -");
+	}
+	
+	public void dado (double tirada) {
+		System.out.println("                                                                                          \r\n"
+				+ "                                        .:----.                                           \r\n"
+				+ "                                   :-===-. -+=**=:                                        \r\n"
+				+ "                             .-=+*#%#%%-   #+==-:--=-.                                    \r\n"
+				+ "                        .-+#@*#%@@**#@#.   @+-:=*@@=.*#+-.                                \r\n"
+				+ "                   .---=-=+-  *+++==:     :#*+=:::+@@@*#@#+-                              \r\n"
+				+ "               :---.  ...::::-------+==-=+- *+=:.   +@# @@@+#*-                           \r\n"
+				+ "            =-==-----::..           .... -* .-*:.-:: .. +%@@%%%%=.                        \r\n"
+				+ "           *...                           :-    ++****=+===++=.:+%#=.                     \r\n"
+				+ "          =:.-      .*%@@#:             ..       ++++++-=-+*+##**+*#@+                    \r\n"
+				+ "         .*  -:    +@@%@@@@#%#:        -+:        **=+++----+-++--+*+##:                  \r\n"
+				+ "         +=   =.  +@@%=@@@#@@@+      :=*.          **-=--.-:-=##*:-==--#=                 \r\n"
+				+ "        .%     +  :@@@@@#:*@@@:    :*+:             *-. :@@%+-+@@#=:---+#:                \r\n"
+				+ "        -=     .+   .%@@@@@#=     -*+:               *-==%@@@@%+#@%*###+=+                \r\n"
+				+ "        +:      .=    :==:.     .*+=.:::              %=+-@@@#@@@@@++**-++                \r\n"
+				+ "        # =**-   ::            -*#*@@@@@@=      :.    .%==-@@#=%%@*+-:.-%:                \r\n"
+				+ "       :*:@%@@.   =          .*#+.*%#*%@@@.  :*@@@@#-  +*::+%@%+-+#=-.*%%                 \r\n"
+				+ "       +-*@#@@.    +        +%*.    =#@@@#  #@@%++@@@:  %+*+**==++*::%##*                 \r\n"
+				+ "       *%%%@@=.    :-     .##*   :#@@@@@*  =@@%   +@@-  :@+*+++*:==.##+@@%*-              \r\n"
+				+ "       ++@@@=+@+    -:   =++-   #@@@@@*:.  *@@%::=@@%    :@**++++..%**=@@@@@@#-           \r\n"
+				+ "       - -=.#@#      +  =+=.   .#%@@@@@@@@:=@@@@@@@%.     +#-+++ .%*==*@@@@@@@@@*.        \r\n"
+				+ "      ::   =@#        ..+=--=--::.    .:--. =#@@@@*.       %=+:.+**=-=%@@@@@@@@@@@*:      \r\n"
+				+ "      +:   %*    .-:-+:=        .:-========--::.-:.       :@ +:=+++*#@@@@@@@@@@@@@@@+     \r\n"
+				+ "      *.              -:                              :----- =.=+*=@@%@@@@@@@@@@@@@@@=    \r\n"
+				+ "      *. :-:.         --                                   --+*%==+@@@@@@@@@@@@@@@@@%-    \r\n"
+				+ "      -+.              =:           :                      *#++#+:-%*@@@@@@@@@@@@@@%-     \r\n"
+				+ "       .=.          %%. #         -@@=                   :+=***%@==##@@@@@@@@@@@@@*.      \r\n"
+				+ "         ==:   -+-  =*: :+.    =##@@@%#*+=:  *@+       :*++=+*+#***+#@@@@@@@@@@@@=        \r\n"
+				+ "           :== .%@=%@@@* .*    =%@@@*#@@@@# *@@-     :+#+#%+++**%%**@@@@@@@@@@@#.         \r\n"
+				+ "             #@+:+@@@=@@* :=    -@@*=%@@*- :@@%    .+*+*#@@@*+*+#@-@@@@@@@@@@@=           \r\n"
+				+ "             :*@@%=-+#%%=  *.   =@@@@%=.   *@@-   +#*@@@@@#@@=%=**#@@@@@@@@@#.            \r\n"
+				+ "               .-#@%=       #.   =*=:      *%=  =**++#@@@%@%*=++#@@@@@@@@@@*              \r\n"
+				+ "                  .+#@*-    .*                -*+**++##***###%@@@@@@@@@@%*-               \r\n"
+				+ "                      :+%+.  .*             =#=+*+*++##%@@@@@@@@@@%*+=-                   \r\n"
+				+ "                         :+*- :           =##*-=+#%@@@@%##*+=-:                           \r\n"
+				+ "                            :==:        =++*###*+=-:.                                     \r\n"
+				+ "                                :-:::-+*==-.                                              \r\n"
+				+ "                                                                                          \r\n"
+				+ "                                                                                      ");
+		if(tirada <= 0.05) {
+			System.out.println("\t\t\t  -  Has sacado un 1  -  \n");
+		}else if(tirada <= 0.10) {
+			System.out.println("\t\t\t  -  Has sacado un 2  -  \n");
+		}else if(tirada <= 0.15) {
+			System.out.println("\t\t\t  -  Has sacado un 3  -  \n");
+		}else if(tirada <= 0.20) {
+			System.out.println("\t\t\t  -  Has sacado un 4  -  \n");
+		}else if(tirada <= 0.25) {
+			System.out.println("\t\t\t  -  Has sacado un 5  -  \n");
+		}else if(tirada <= 0.30) {
+			System.out.println("\t\t\t  -  Has sacado un 6  -  \n");
+		}else if(tirada <= 0.35) {
+			System.out.println("\t\t\t  -  Has sacado un 7  -  \n");
+		}else if(tirada <= 0.40) {
+			System.out.println("\t\t\t  -  Has sacado un 8  -  \n");
+		}else if(tirada <= 0.45) {
+			System.out.println("\t\t\t  -  Has sacado un 9  -  \n");
+		}else if(tirada <= 0.50) {
+			System.out.println("\t\t\t  -  Has sacado un 10  -  \n");
+		}else if(tirada <= 0.55) {
+			System.out.println("\t\t\t  -  Has sacado un 11  -  \n");
+		}else if(tirada <= 0.60) {
+			System.out.println("\t\t\t  -  Has sacado un 12  -  \n");
+		}else if(tirada <= 0.65) {
+			System.out.println("\t\t\t  -  Has sacado un 13  -  \n");
+		}else if(tirada <= 0.70) {
+			System.out.println("\t\t\t  -  Has sacado un 14  -  \n");
+		}else if(tirada <= 0.75) {
+			System.out.println("\t\t\t  -  Has sacado un 15  -  \n");
+		}else if(tirada <= 0.80) {
+			System.out.println("\t\t\t  -  Has sacado un 16  -  \n");
+		}else if(tirada <= 0.85) {
+			System.out.println("\t\t\t  -  Has sacado un 17  -  \n");
+		}else if(tirada <= 0.90) {
+			System.out.println("\t\t\t  -  Has sacado un 18  -  \n");
+		}else if(tirada <= 0.95) {
+			System.out.println("\t\t\t  -  Has sacado un 19  -  \n");
+		}else if(tirada <= 1) {
+			System.out.println("\t\t\t  -  Has sacado un 20  -  \n");
+		}
+	}
+	
+	public void lanzaDados () {
+		System.out.println("\t\t\t  -  Lanza el dado  -\n");
+	}
+	
+	public void ataqueAcertado (int dmg, Enemigo boss) {
+		System.out.println("\t\t\t  -  Tu ataque acierta  -");
+		System.out.println("\t\t\t  -  "+boss.getNombre()+" recibe "+dmg+" de daño  -\n");
+	}
+	
+	public void imprimirString (String frase) {
+		System.out.println("\t\t\t  -  " +frase+ "  -  \n");
+	}
+	
+	public void cambiarArma (Arma armaEquipada) {
+		System.out.println("\t\t\t  -  Ahora estas equipado con " +armaEquipada.getNombre());
 	}
 	
 	public void menuArmas (Jugador j1) {
